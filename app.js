675307aa0b190da4520414b282ca015a35411c14
abhisheksharma <abhisheksharma131098@gmail.com>
2023-04-21 11:27:42 +0530

    file modified

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
-const date = require(__dirname + "/date.js");
+//const date = require(__dirname + "/date.js");
 require('dotenv').config();
 
 //Initiallize express as app
